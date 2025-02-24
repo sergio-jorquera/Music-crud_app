82781b9089fcc9d454b4419509d1d8cec65f3d2b
NatKaz <astureparaciondiseno@gmail.com>
2025-02-24 14:39:06 +0100

    Get commit

---
+++ src/service.js
@@ -0,0 +1,19 @@
+const API_URL= "http://localhost:3000/musica"
+document.getElementById("load-music").addEventListener("click",listMusic);
+
+
+
+
+
+async function listMusic() {
+try { 
+const response = await fetch(API_URL);
+const data= await response.json();
+console.log("Musica obtenida:",data)
+return data;
+
+} catch (error) {
+    console.error("Error obteniendo musica:", error);
+}
+}
+listMusic();
