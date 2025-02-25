4b78f7b4c920ae43d2985206c4b8342aa96b4d83
sergio-jorquera <sjorkera@gmail.com>
2025-02-25 09:16:58 +0100

    methodo_PRINT

--- src/service.js
+++ src/service.js
@@ -1,5 +1,5 @@
 const API_URL= "http://localhost:3000/musica"
-document.getElementById("load-music").addEventListener("click",listMusic);
+document.getElementById("load-music").addEventListener("click", printListMusic);
 
 
 
@@ -17,28 +17,31 @@ return data;
 }
 }
 
-// async function printListMusic(){
-//         const playList = await listMusic();
-//         const tbody = document.querySelector("#music-body");
-//         if(!tbody){
-//             console.error("No se encontro el tbody en html");
-//             return;
-//         }
-//         tbody.innerHTML = "";
+async function printListMusic(){
+        const playList = await listMusic();
+        const tbody = document.querySelector("#music-body");
+        if(!tbody){
+            console.error("No se encontro el tbody en html");
+            return;
+        }
+        tbody.innerHTML = "";
 
-//         playList.forEach((song) => {
-//             console.log(`Agregando cancion:${musica.id}-${musica.titulo}`);
-//             tbody.insertAdjacentElement(
-//                 "beforeend",
-//                 `<tr>
-//                 <td>${musica.id}</td>
-//                 <td>${musica.titulo}</td>
-//                 <td>${musica.grupo}</td>
-//                 <td>${musica.album}</td>
-//                 <td>${musica.año}</td>
-//                 <td><button onclick ="print
-//                 `
-//             )
-//         })
+        playList.forEach((song) => {
+            console.log(`Agregando cancion:${song.id}-${song.titulo}`);
+            const row = document.createElement("tr");
+            row.innerHTML = `
+              <td>${song.id}</td>
+              <td>${song.title}</td>
+              <td>${song.group}</td>
+              <td>${song.album}</td>
+              <td>${song.year}</td>
+              <td><button onclick="openEditForm('${song.id}')">Modificar</button></td>
+              <td><button onclick="deleteFilm('${song.id}')">Delete</button></td>
+            `;
+            tbody.appendChild(row);
+          });
         
-// }
+          console.log("Tabla después de actualizar:", tbody.innerHTML); 
+        }
+
+  
